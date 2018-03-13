24f48d8575a1d373e766c4fd67e7047187ed2e75
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 14:23:33 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
+# def square_array(array)
+#   new_array = []
+#   array.each do |number|
+#     new_array << number ** 2
+#   end
+#   return new_array
+# end
+
 def square_array(array)
-  new_array = []
-  array.each do |number|
-    new_array << number ** 2
+  array.collect do |number|
+    number ** 2
   end
-  return new_array
 end
